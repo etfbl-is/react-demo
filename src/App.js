eed84c011fa0b79db06f5f8ce28d8396ad37a25d
Djordje Turjacanin <etfbl.is@gmail.com>
2022-01-16 15:57:10 +0100

    Dependent select component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,27 @@
 import { useEffect, useState } from "react";
-import { fakeApiCall } from "./api.service";
+import { fakeApiCall, getCities } from "./api.service";
 import "./App.css";
 
 function App() {
   const [counter, setCounter] = useState(0);
   const [data, setData] = useState([]);
+  const [countries] = useState([
+    {
+      id: "BIH",
+      name: "Bosna i Hercegovina",
+    },
+    {
+      id: "SRB",
+      name: "Srbija",
+    },
+    {
+      id: "CRO",
+      name: "Hrvatska",
+    },
+  ]);
+  const [cities, setCities] = useState([]);
+  const [country, setCountry] = useState("BIH");
+  const [city, setCity] = useState(null);
   const onClearData = () => {
     setData([]);
   };
@@ -14,6 +31,13 @@ function App() {
   useEffect(() => {
     fakeApiCall(counter).then((data) => setData(data));
   }, [counter]);
+  useEffect(() => {
+    getCities(country).then((res) => {
+      if (res.length > 0) setCity(res[0]?.id);
+      else setCity(null);
+      setCities(res);
+    });
+  }, [country]);
   return (
     <div>
       <h1
@@ -22,6 +46,34 @@ function App() {
       >
         Current count is {counter}
       </h1>
+      <div className="dependency-select">
+        <select
+          value={country}
+          onChange={(e) => {
+            e.preventDefault();
+            setCountry(e.target.value);
+          }}
+        >
+          {countries.map((country) => (
+            <option key={country.id} value={country.id}>
+              {country.name}
+            </option>
+          ))}
+        </select>
+        <select
+          value={city}
+          onChange={(e) => {
+            e.preventDefault();
+            setCity(e.target.value);
+          }}
+        >
+          {cities.map((city) => (
+            <option key={city.id} value={city.id}>
+              {city.name}
+            </option>
+          ))}
+        </select>
+      </div>
       <div className="counter-input">
         <input
           type="number"
--- src/api.service.js
+++ src/api.service.js
@@ -8,3 +8,55 @@ export const fakeApiCall = (count) => {
     });
   return Promise.resolve(array);
 };
+
+export const getCities = (country) => {
+  const citiesMap = {
+    BIH: [
+      {
+        id: "78000",
+        name: "Banja Luka",
+      },
+      {
+        id: "78430",
+        name: "Prnjavor",
+      },
+      {
+        id: "78400",
+        name: "Gradiška",
+      },
+      {
+        id: "74000",
+        name: "Doboj",
+      },
+    ],
+    SRB: [
+      {
+        id: "11000",
+        name: "Beograd",
+      },
+      {
+        id: "21000",
+        name: "Novi Sad",
+      },
+      {
+        id: "18000",
+        name: "Niš",
+      },
+    ],
+    CRO: [
+      {
+        id: "10000",
+        name: "Zagreb",
+      },
+      {
+        id: "21000",
+        name: "Split",
+      },
+      {
+        id: "51000",
+        name: "Rijeka",
+      },
+    ],
+  };
+  return Promise.resolve(citiesMap[country]);
+};
